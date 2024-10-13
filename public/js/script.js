0876aab828f457ed17ed9e6fd75003b30c0095bd
razor-boi <sunandandhiman466@gmail.com>
2024-10-13 16:33:28 -0700

    nodejs project  first commit

---
+++ public/js/script.js
@@ -0,0 +1,53 @@
+const search_button = document.getElementById("search_button");
+const search = document.getElementById("search_bar_id");
+const p_tag = document.getElementById('p_tag');
+const temperature = document.getElementById("temperature")
+const getinfo = async (event)=>{
+    event.preventDefault();
+    let cityname = search.value;
+    if(cityname === ''){
+        p_tag.innerText = `pls write the name before search`;
+    }
+    else{
+        try{            
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=565df217d8a66df72d4e8582e7eb2c7e`
+
+            const response = await fetch(url);
+            const data = await response.json();
+            
+            const arrData = [data];
+
+            //  temperature.innerText = arrData[0].main.temp -273.15;
+            //  let b = math.round(a*10)/10;
+                let  t = arrData[0].main.temp -273.15;
+              let b = Math.round(t * 10) / 10;
+              temperature.innerText = b;
+             p_tag.innerText=` ${cityname}'s Weather`;
+            let weather_value = arrData[0].weather[0].main ;
+            let weather_image = document.getElementById('weather-picture');
+            if(weather_value == 'Clouds'){
+                // alert('cloudy day')
+weather_image.src = '../css/images/cloudpic.png';
+            }
+            else{
+                // alert("it's not cloudy")
+                weather_image.src = "../css/images/sunpic.jpg";
+            }
+            console.log(weather_value);
+
+            console.log(arrData);
+        }
+        catch{
+            p_tag.innerText = `pls write the city name properly`;
+        }
+
+
+        
+        
+
+        
+    }
+}
+    
+
+search_button.addEventListener('click',getinfo);
---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");``
+const app = express();
+const port = process.env.PORT || 8000 ;
+const path= require("path");
+const hbs = require("hbs");
+
+// Public Static Path
+const staticpath = (path.join(__dirname ,"../public"))
+const template_path = (path.join(__dirname ,"../src/views"))
+// const partials_path = (path.join(__dirname ,"../src/partials"));
+
+// Setting up view engine
+app.set('view engine','hbs')
+// app.set('views',template_path);
+app.use(express.static(staticpath));
+
+
+
+// hbs.registerPartials(partials_path);
+
+// Routing
+app.get("/" ,(req,res)=>{
+    res.render("index");
+})
+app.get("/about",(req,res)=>{
+    res.render('about');
+})
+app.get('/weather' ,(req,res)=>{
+    res.render('weather');
+})
+
+app.listen(port ,(req,res)=>{
+    console.log(`Listening at the port ${port}`);
+})
